4af597bd4192aa339892cb70e76cb5e871032230
jackli0523 <j2379438361@gmail.com>
2023-12-04 21:28:21 -0800

    added UI

---
+++ Assets/Scripts/UI/TrasformUIPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrasformUIPanel : MonoBehaviour
+{
+    public GameObject Panel;
+
+    private void Start()
+    {
+        GameManager.Instance.Continue();
+        Panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Panel.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            Panel.SetActive(false);
+        }
+    }
+}
